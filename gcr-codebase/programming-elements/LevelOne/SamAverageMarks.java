f6722d0bb83be8260fa9bdf420a24a8065e7278e
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-17 16:27:24 +0530

    [Harsh]: Added Sam Average Marks Problem

---
+++ gcr-codebase/programming-elements/LevelOne/SamAverageMarks.java
@@ -0,0 +1,12 @@
+public class SamAverageMarks{
+	public static void main(String[] args){
+	//Here are the Obtained marks of Sam in Physics, Maths and Chemistry.
+	  double PhysicsMarks = 95;
+	  double ChemistryMarks = 96;
+	  double MathsMarks = 94;
+	  //Calculating the Average marks in PCM.
+	  double AverageMarks = ((PhysicsMarks + ChemistryMarks + MathsMarks)/3);
+	  System.out.println("Sam's average mark in PCM is "+AverageMarks);
+	  
+	}
+}
