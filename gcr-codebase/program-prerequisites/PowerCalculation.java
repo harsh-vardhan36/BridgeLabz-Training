ed60a68e7c69889b52d268bd5cf48b773e87ea18
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-17 13:35:56 +0530

    [Harsh]: Added self practice-> Power Calculation program

---
+++ gcr-codebase/program-prerequisites/PowerCalculation.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ public class PowerCalculation{
+	public static void main(String [] args){
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter the base number: ");
+	double base = in.nextDouble();
+	System.out.println("Enter the Exponent: ");
+	double Exponent = in.nextDouble();
+	double Result = Math.pow(base, Exponent);
+	System.out.println("The result is: "+result);
+	in.close();
+	
+	
+	}
+ }
