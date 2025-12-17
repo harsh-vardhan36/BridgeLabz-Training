9fefc0c29590dfa6c7e2334a6a3011ebbe46fad4
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-17 16:38:05 +0530

    [Harsh]: Added LevelOne -> Kilometer to Miles Program

---
+++ gcr-codebase/programming-elements/LevelOne/ConvertKmtoMiles.java
@@ -0,0 +1,10 @@
+public class ConvertKmtoMiles{
+	public static void main(String[] args){
+	//Creating a double variable to store the Kilometer
+	double Kilometer = 10.8;
+	//It is given 1 mile equals 1.6 kilometer
+	double Miles = 10.8/1.6;
+	System.out.println("The distance "+Kilometer+" km in miles is "+Miles);
+	
+	}
+}
