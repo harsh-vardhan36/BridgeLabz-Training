4eedfb11ff72d6c9a104113d374c92c362f8a432
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-17 13:15:17 +0530

    [Harsh]: Added self practice -> perimeter of rectangle program

---
+++ gcr-codebase/program-prerequisites/PerimeterofRectangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PerimeterofRectangle{
+	public static void main(String [] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter Length");
+	int length = in.nextInt();
+	System.out.print("Enter Width: ");
+	int breadth = in.nextInt();
+	in.close();
+	System.out.println((2*(length + breadth));
+	
+	}
+}
