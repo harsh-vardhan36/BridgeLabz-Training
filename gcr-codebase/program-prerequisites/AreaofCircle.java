ead90ce2278a66a67982abdc84f9d6ccf4a20f49
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-17 10:31:26 +0530

    [Harsh]: added Area of Circle Program

---
+++ gcr-codebase/program-prerequisites/AreaofCircle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class AreaofCircle
+{
+public static void main(String [] args)
+{
+Scanner in = new Scanner(System.in);
+int radius = in.nextInt();
+int area = ((22/7)*radius*radius);
+System.out.println(area);
+}
+}
