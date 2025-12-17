92f1240efa99ef3e38863eb772df243a8af78eac
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-17 11:20:53 +0530

    [Harsh]: Added Sum of two Number

---
+++ gcr-codebase/program-prerequisites/addNum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class AddNum
+{
+public static void main(String [] args)
+{
+Scanner in = new Scanner(System.in);
+int firstVar = in.nextInt(); //Declaring the variables.
+int secondVar = in.nextInt();
+System.out.println(firstVar+secondVar);
+
+}
+}
