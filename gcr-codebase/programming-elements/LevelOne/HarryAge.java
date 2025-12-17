58f523047cdf03cf8960b842be123a8f9a160667
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-17 16:08:11 +0530

    [Harsh]: Added levelOne -> Harry Current age program

---
+++ gcr-codebase/programming-elements/LevelOne/HarryAge.java
@@ -0,0 +1,11 @@
+public class HarryAge{
+	public static void main(String[] args){
+	//Creating a variable to store current year.
+	   int CurrentYear = 2025;
+	//Creating a variable to store current year.
+	   int BirthYear = 2000;
+	//Calculating the current age of Harry.
+	   int age = CurrentYear - BirthYear;
+	 System.out.println("Harry's age in "+CurrentYear+" is "+age);
+	}
+}
