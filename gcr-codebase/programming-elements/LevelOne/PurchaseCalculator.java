bb69915f0766622c38863c8c5e61e798f5dbf8d2
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-18 11:34:07 +0530

    [Harsh]: Added LevelOne -> Purchase Calculator program

---
+++ gcr-codebase/programming-elements/LevelOne/PurchaseCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PurchaseCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter unit price (INR): ");
+        double unitPrice = sc.nextDouble();
+
+        System.out.print("Enter quantity: ");
+        int quantity = sc.nextInt();
+
+        // Calculation
+        double totalPrice = unitPrice * quantity;
+
+        // Output
+        System.out.println("The total purchase price is INR " + totalPrice +
+                           " if the quantity " + quantity +
+                           " and unit price is INR " + unitPrice);
+    }
+}
