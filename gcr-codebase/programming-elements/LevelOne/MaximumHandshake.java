8f1750809fd99aeb7cda4965de2ebb1343a9d69c
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-18 12:12:19 +0530

    [Harsh]: Added Maximum Handshake program -> LevelOne

---
+++ gcr-codebase/programming-elements/LevelOne/MaximumHandshake.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//Creating a class PurchaseCalculator to Calculate the Total price.
+public class MaximumHandshake {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Taking Input in variable 
+        System.out.print("Enter the number of students");
+		int numberOfStudents = input.nextInt();
+		
+		//Calculating the Possible Number of Handshake
+		int maximumHandshake = ((numberOfStudents*(numberOfStudents -1))/2);
+		
+		//Displaying the Possible Handshakes.
+		System.out.println(maximumHandshake);
+		
+    }
+}
