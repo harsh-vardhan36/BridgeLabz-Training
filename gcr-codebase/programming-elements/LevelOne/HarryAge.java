35bd6f29c4fd07dd5e8d88305a67792340bcc99c
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-18 12:14:59 +0530

    [Harsh]: Updated the CurrentYear Variable -> LevelOne

--- gcr-codebase/programming-elements/LevelOne/HarryAge.java
+++ gcr-codebase/programming-elements/LevelOne/HarryAge.java
@@ -1,7 +1,7 @@
 public class HarryAge{
 	public static void main(String[] args){
 	//Creating a variable to store current year.
-	   int CurrentYear = 2025;
+	   int CurrentYear = 2024;
 	//Creating a variable to store current year.
 	   int BirthYear = 2000;
 	//Calculating the current age of Harry.
