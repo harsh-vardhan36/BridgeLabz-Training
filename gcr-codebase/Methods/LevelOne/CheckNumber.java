1c92260ed95bc840ef9bf044fb7a0fcc2a897a80
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-20 15:50:56 +0530

    [Harsh Vardhan]: Added Check number methods

---
+++ gcr-codebase/Methods/LevelOne/CheckNumber.java
@@ -0,0 +1,27 @@
+///Program to Check whether a number is negative, positive or zero.
+import java.util.Scanner;
+public class CheckNumber{
+	public static void main(String [] args){
+	
+	Scanner in = new Scanner(System.in);
+	
+	//Creating object of CheckNumber
+	CheckNumber CN = new CheckNumber();
+	
+	//Taking the variable from user.
+	System.out.print("Enter the number: ");
+	int number = in.nextInt();
+	
+	//Calling the method in object.
+	System.out.println(CN.CheckNumber(number));
+	}
+	
+	//Declaring a Method. 
+	
+	public int CheckNumber(int number){
+		if(number>0) return 1;
+		else if (number ==0) return 0;
+		else return -1;
+	  
+	}
+}
