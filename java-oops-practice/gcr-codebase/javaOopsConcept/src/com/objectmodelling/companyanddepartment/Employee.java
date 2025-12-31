526affbbb8a75121363c70f7e1d4d3eea35e55ff
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2025-12-31 10:39:32 +0530

    [Harsh Vardhan]: Added Company and Department

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/objectmodelling/companyanddepartment/Employee.java
@@ -0,0 +1,22 @@
+package com.objectmodelling.companyanddepartment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Employee class
+public class Employee {
+    private String name;
+    private String role;
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public void displayEmployee() {
+        System.out.println("Employee: " + name + " | Role: " + role);
+    }
+}
+
+
+
