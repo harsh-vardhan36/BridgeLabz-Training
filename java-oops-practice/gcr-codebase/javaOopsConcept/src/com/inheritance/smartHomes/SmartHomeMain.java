2b8a9fd03bf18f8d9e0226b70948adbf89066cb2
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-01 14:58:53 +0530

    [Harsh Vardhan]: Added Smart Homes Device program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/inheritance/smartHomes/SmartHomeMain.java
@@ -0,0 +1,9 @@
+package com.inheritance.smartHomes;
+
+public class SmartHomeMain {
+	  public static void main(String[] args) {
+
+	        Thermostat thermostat = new Thermostat("TH101", "ON", 24);
+	        thermostat.displayStatus();
+	    }
+}
