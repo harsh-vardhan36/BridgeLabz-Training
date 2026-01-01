3cfd4200335688ef2ffd7400cc65064c123dcd85
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-01 13:23:50 +0530

    [Harsh Vardhan]: Added Animal Hierarchy program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/inheritance/animalhierarchy/AnimalHierarchy.java
@@ -0,0 +1,46 @@
+package com.inheritance.animalhierarchy;
+
+class Animal{
+	String name;
+	int age;
+	
+	void makeSound() {
+		System.out.println("Animals make sound");
+	}
+}
+
+class Dog extends Animal{
+	void makeSound() {
+		System.out.println("Dog make sound");
+	}
+}
+
+class Cat extends Animal{
+	void makeSound() {
+		System.out.println("Cat make sound");
+	}
+}
+
+class Bird extends Animal{
+	void makeSound() {
+		System.out.println("Bird make sound");
+	}
+}
+
+public class AnimalHierarchy {
+
+	public static void main(String[] args) {
+		
+		Bird bird = new Bird();
+		Cat cat = new Cat();
+		Dog dog = new Dog();
+		Animal animal = new Animal();
+		
+		bird.makeSound();
+		cat.makeSound();
+		dog.makeSound();
+		animal.makeSound();
+
+	}
+
+}
