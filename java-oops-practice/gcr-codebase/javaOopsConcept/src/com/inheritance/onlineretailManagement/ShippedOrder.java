bc01a74e344c5f152186c3b4f3ecb63b39cdd4e4
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-01 14:42:39 +0530

    [Harsh Vardhan]: added retail Management System

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/inheritance/onlineretailManagement/ShippedOrder.java
@@ -0,0 +1,16 @@
+package com.inheritance.onlineretailManagement;
+
+public class ShippedOrder extends Order {
+
+    protected String trackingNumber;
+
+    public ShippedOrder(int orderId, String orderDate, String trackingNumber) {
+        super(orderId, orderDate);
+        this.trackingNumber = trackingNumber;
+    }
+
+    @Override
+    public String getOrderStatus() {
+        return "Order Shipped";
+    }
+}
