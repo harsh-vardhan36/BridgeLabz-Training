ee3c62338648b5363fd7ede36a15b3873c9dec7b
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-01 13:36:41 +0530

    [Harsh Vardhan]: Added Employee ManagementSystem

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/inheritance/employeemanagement/EmployeeMain.java
@@ -0,0 +1,20 @@
+package com.inheritance.employeemanagement;
+
+public class EmployeeMain {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp = new Employee("ABC",18,2500);
+		
+		Manager mng = new Manager("Priyanshu",21,5000);
+		
+		Developer dvp = new Developer("Sid",45,4100);
+		
+		Intern inr  = new Intern("DJ",23,5600);
+		
+		emp.displaydetails();
+		mng.displaydetails();
+		dvp.displaydetails();
+		inr.displaydetails();
+		
+	}
+}
