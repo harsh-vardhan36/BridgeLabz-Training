76e3ac315e10a57b580c1bc12fcf2846afffbf11
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-05 02:20:44 +0530

    [Harsh Vardhan]: Added Queue USing Stack program

---
+++ java-dsa-practice/gcr-codebase/com/src/com/queue/QueueUsingStack/Main.java
@@ -0,0 +1,20 @@
+package com.queue;
+
+public class Main {
+    public static void main(String[] args) {
+
+        QueueUsingStacks queue = new QueueUsingStacks();
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+
+        queue.enqueue(40);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+    }
+}
---
+++ java-dsa-practice/gcr-codebase/com/src/com/queue/QueueUsingStack/QueueUsingStacks.java
@@ -0,0 +1,33 @@
+package com.queue;
+
+import java.util.Stack;
+
+public class QueueUsingStacks {
+
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public QueueUsingStacks() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void enqueue(int data) {
+        stack1.push(data);
+    }
+
+    public int dequeue() {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        return stack2.pop();
+    }
+}
