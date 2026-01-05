e5f6af57e411c4f2f35e94cdcb7e28ba1b7d1bd5
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-05 10:55:08 +0530

    [Harsh Vardhan]: Added Hashmap Program ZeroSumSubarrays.

---
+++ java-dsa-practice/gcr-codebase/com/src/com/hashmap/subarray/Main.java
@@ -0,0 +1,9 @@
+package com.hashmap.subarray;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {1, -1, 2, -2, 3};
+        ZeroSumSubarrays.findSubarrays(arr);
+    }
+}
