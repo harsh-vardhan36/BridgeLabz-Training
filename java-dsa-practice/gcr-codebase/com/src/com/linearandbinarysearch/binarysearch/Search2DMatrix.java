ea4db75e67a81c3678e0d46888119929daf4467e
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-08 11:13:32 +0530

    [Harsh Vardhan]: Added Search2DMatrix program using Binary Search

---
+++ java-dsa-practice/gcr-codebase/com/src/com/linearandbinarysearch/binarysearch/Search2DMatrix.java
@@ -0,0 +1,47 @@
+package com.linearandbinarysearch.binarysearch;
+
+public class Search2DMatrix {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            // Convert 1D index to 2D indices
+            int row = mid / cols;
+            int col = mid % cols;
+
+            int midValue = matrix[row][col];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+
+        int target = 16;
+
+        System.out.println(searchMatrix(matrix, target));
+    }
+}
