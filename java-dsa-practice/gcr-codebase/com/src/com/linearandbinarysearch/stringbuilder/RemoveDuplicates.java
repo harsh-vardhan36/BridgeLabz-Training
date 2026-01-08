9540eb1ac816b776bcd518d72a490cc236be2f72
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-08 10:38:13 +0530

    [Harsh Vardhan]: Added RemoveDuplicates program using String Builder

---
+++ java-dsa-practice/gcr-codebase/com/src/com/linearandbinarysearch/stringbuilder/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+package com.linearandbinarysearch.stringbuilder;
+import java.util.HashSet;
+public class RemoveDuplicates {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		HashSet<Character> hn = new HashSet<>();
+		
+		String str = "HarshVardhan";
+		
+		for(char ch : str.toCharArray()) {
+			if(hn.contains(ch) != true) {
+				sb.append(ch);
+				hn.add(ch);
+			}
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+
+
+}
