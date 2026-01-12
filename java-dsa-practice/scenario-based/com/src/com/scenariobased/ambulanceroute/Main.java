15137b7c003d7d9b149afdf249a2b263d14a431a
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-12 11:23:46 +0530

    [Harsh Vardhan]: Added AmbulanceRoute Scenario Based program

---
+++ java-dsa-practice/scenario-based/com/src/com/scenariobased/ambulanceroute/Main.java
@@ -0,0 +1,22 @@
+package com.scenariobased.ambulanceroute;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AmbulanceRoute route = new AmbulanceRoute();
+
+        route.addUnit("Emergency");
+        route.addUnit("Radiology");
+        route.addUnit("Surgery");
+        route.addUnit("ICU");
+
+        route.setAvailability("Emergency", false);
+        route.setAvailability("Radiology", false);
+
+        route.sendPatient();   
+
+        route.removeUnit("ICU");
+
+        route.sendPatient();   
+    }
+}
---
+++ java-dsa-practice/scenario-based/com/src/com/scenariobased/ambulanceroute/Unit.java
@@ -0,0 +1,11 @@
+package com.scenariobased.ambulanceroute;
+
+class Unit {
+    String name;
+    boolean available = true;
+    Unit next;
+
+    Unit(String name) {
+        this.name = name;
+    }
+}
