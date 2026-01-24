131ef2bcd2c73a610d754bd11b776ed6d0903c34
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-24 14:46:02 +0530

    [Harsh vardhan]: Added multiple catch program for ArrayException in java-collections in multiple catch

---
+++ java-collections-practice/gcr-codebase/com/src/com/exceptionhandling/multiplecatch/ArrayException.java
@@ -0,0 +1,30 @@
+package com.exceptionhandling.multiplecatch;
+
+import java.util.Scanner;
+
+public class ArrayException{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            // You can change this to null to test NullPointerException
+            int[] arr = {10, 20, 30, 40, 50};
+
+            System.out.print("Enter index: ");
+            int index = sc.nextInt();
+
+            System.out.println("Value at index " + index + ": " + arr[index]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+
+        } finally {
+            sc.close();
+        }
+    }
+}
