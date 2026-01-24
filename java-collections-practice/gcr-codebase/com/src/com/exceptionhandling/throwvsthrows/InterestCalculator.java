6c90197d381d072f0197d768c97e9ec8adc1e114
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-24 15:31:00 +0530

    [Harsh Vardhan]: Added Interest calculator program using throwvsThrows in java collections

---
+++ java-collections-practice/gcr-codebase/com/src/com/exceptionhandling/throwvsthrows/InterestCalculator.java
@@ -0,0 +1,28 @@
+package com.exceptionhandling.throwvsthrows;
+
+public class InterestCalculator {
+
+    // Method that propagates exception
+    static double calculateInterest(double amount, double rate, int years)
+            throws IllegalArgumentException {
+
+        if (amount < 0 || rate < 0) {
+            throw new IllegalArgumentException(
+                "Invalid input: Amount and rate must be positive"
+            );
+        }
+
+        return (amount * rate * years) / 100;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            double interest = calculateInterest(10000, 5, 2);
+            System.out.println("Calculated Interest: " + interest);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
