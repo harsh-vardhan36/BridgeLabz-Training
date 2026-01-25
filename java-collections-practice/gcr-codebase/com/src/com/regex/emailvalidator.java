329e9dc26de9b3644787215cc4d5ac8441e171af
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-01-25 15:56:46 +0530

    [Harsh Vardhan]: Added email Validator program using regex

--- java-collections-practice/gcr-codebase/com/src/com/regex/emailvalidator.java
+++ java-collections-practice/gcr-codebase/com/src/com/regex/emailvalidator.java
@@ -1,30 +1,36 @@
 package com.regex;
 import java.util.*;
-public class emailvalidator{
 
-	public static void main(String [] args) {
-		Scanner in = new Scanner(System.in);
-		boolean isTrue = true;
-		String username;
-		String regex = "^[a-zA-Z][a-zA-Z0-9@a-z.a-z]{4,14}$";
-		char val;
-		while(isTrue) {
-		System.out.println("Enter the username: ");
-		username = in.nextLine();
-		
-		
-		if(username.matches(regex)) {
-			System.out.println("Verified!");
-		}
-		else {
-			System.out.println("Not Verified!");
-		}
-		System.out.println("Do you want to check the Username again: y or n ");
-		val = in.next().charAt(0);
-		if( val == 'y') {
-			isTrue = true;
-		}
-		isTrue = false;
-		}
-	}
+public class EmailValidator {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        boolean isTrue = true;
+
+        String regex = "^[a-zA-Z][a-zA-Z0-9_]{4,14}$";
+
+        while (isTrue) {
+
+            System.out.println("Enter the username: ");
+            String username = in.nextLine();
+
+            if (username.matches(regex)) {
+                System.out.println("Verified!");
+            } else {
+                System.out.println("Not Verified!");
+            }
+
+            System.out.println("Do you want to check the Username again? (y/n)");
+            char val = in.next().charAt(0);
+            in.nextLine(); // consume newline
+
+            if (val == 'y') {
+                isTrue = true;
+            } else {
+                isTrue = false;
+            }
+        }
+        in.close();
+    }
 }
