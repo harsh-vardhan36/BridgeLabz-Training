b3a800e40abebb6ead72b3df2c77bbbde8e2f861
harsh-vardhan36 <harsh15vardhan457@gmail.com>
2026-02-02 02:35:59 +0530

    [Harsh Vardhan]: Added logging transcations program in java8

---
+++ gcr-codebase/com/src/com/java8/stream/loggingtranscations/TransactionLogger.java
@@ -0,0 +1,20 @@
+package com.java8.stream.loggingtranscations;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class TransactionLogger {
+
+    public static void main(String[] args) {
+
+        List<String> transactionIds = List.of(
+            "TXN1001",
+            "TXN1002",
+            "TXN1003"
+        );
+
+        transactionIds.forEach(id ->
+            System.out.println(LocalDateTime.now() + " - Transaction: " + id)
+        );
+    }
+}
